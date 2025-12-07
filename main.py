866b46b828d9bd689c7b25ee4ccb700e9efa43db
Tanmay <nnon92333@gmail.com>
2025-12-07 15:57:02 +0530

    monitoring active window

--- main.py
+++ main.py
@@ -1,19 +1,42 @@
 import webview
-import threading
+import pygetwindow as gw
 import time
+import threading
+
+class Api:
+    def get_current_state(self):
+
+        try:
+            # Get the active window
+            window = gw.getActiveWindow()
+            if window:
+                title = window.title.lower()
+                
+                if 'code' in title or 'studio' in title or 'py' in title:
+                    return 'coding'  # State 1: Working hard
+                elif 'spotify' in title or 'music' in title:
+                    return 'music'   # State 2: Jamming
+                elif 'chrome' in title or 'edge' in title or 'firefox' in title or 'brave' in title:
+                    return 'browsing' # State 3: Internet
+                
+            return 'idle'
+            
+        except Exception as e:
+            return 'idle'
 
 def create_pet_window():
+    api = Api()
+
     window = webview.create_window(
-        'DesktopPet', 
+        'FocusPet', 
         'assets/index.html',
-        transparent=True,   
+        js_api=api,
+        transparent=True,
         frameless=True,      
         on_top=True,         
-        width=200,          
+        width=200,           
         height=200,
-        background_color='#00000000' 
     )
-    
     webview.start()
 
 if __name__ == '__main__':
