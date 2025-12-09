dbc8a9af8d68dd9cc1dcb93b1a3be81a37fa37a1
Tanmay <nnon92333@gmail.com>
2025-12-09 21:59:45 +0530

    tested pystray working

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import webview
 import pygetwindow as gw
 import time
 import threading
+import pystray
+from PIL import Image, ImageDraw
+
 
 class Api:
     def get_current_state(self):
@@ -39,5 +42,25 @@ def create_pet_window():
     )
     webview.start()
 
+def create_image(width, height, color1, color2):
+    # Generate an image and draw a pattern
+    image = Image.new('RGB', (width, height), color1)
+    dc = ImageDraw.Draw(image)
+    dc.rectangle(
+        (width // 2, 0, width, height // 2),
+        fill=color2)
+    dc.rectangle(
+        (0, height // 2, width // 2, height),
+        fill=color2)
+
+    return image
+
+icon = pystray.Icon(
+    'testname',
+    icon=create_image(64, 64, 'black', 'white'))
+
+
+
 if __name__ == '__main__':
     create_pet_window()
+    icon.run()
